952c6a22b260bd47fa017c2c132eee9b39e16d5e
Kevin Cai <kevincai1979@gmail.com>
2018-02-27 02:15:27 -0800

    design components

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyCcpI023TrhR7YnHM0-DxlohTo3sHpkkVE';
+
+const App = () => {
+  return <div>Hi! there</div>;
+};
+
+// React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));

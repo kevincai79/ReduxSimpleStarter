d89c6a77911856143de984df92995a1d8b85b695
Kevin Cai <kevincai1979@gmail.com>
2018-02-27 03:03:30 -0800

    set up input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCcpI023TrhR7YnHM0-DxlohTo3sHpkkVE';
 
 const App = () => {
-  return <div>Hi! there</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // React.render(App);

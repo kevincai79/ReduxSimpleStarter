b14c852aa52b3c7a1d7b8613a545bbc1c3009155
Kevin Cai <kevincai1979@gmail.com>
2018-02-27 11:29:59 -0800

    config youtube search set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCcpI023TrhR7YnHM0-DxlohTo3sHpkkVE';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // React.render(App);
 ReactDOM.render(<App />, document.querySelector('.container'));

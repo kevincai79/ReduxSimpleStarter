8f8a063f5193cf6f9ea70c75ae01127fb328c6a7
Kevin Cai <kevincai1979@gmail.com>
2018-02-27 17:00:37 -0800

    show video list item

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCcpI023TrhR7YnHM0-DxlohTo3sHpkkVE';
 
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }

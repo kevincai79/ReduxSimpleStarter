f1a7a31a17b3905372c90ead6f2bc17e972b4db1
Kevin Cai <kevincai1979@gmail.com>
2018-02-27 22:30:05 -0800

    show youtube video on the page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 
 const API_KEY = 'AIzaSyCcpI023TrhR7YnHM0-DxlohTo3sHpkkVE';
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
